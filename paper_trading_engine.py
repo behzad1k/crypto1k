ddc85d93804ea1e8551ee4cbe306ebe2fb137c66
behzad 1k <bhzd1k@gmail.com>
2025-10-30 11:43:37 +0330

    paper debug

--- paper_trading_engine.py
+++ paper_trading_engine.py
@@ -1041,8 +1041,8 @@ class PaperTradingEngine:
   def process_new_signal(self, signal: Dict):
     """Process a new signal from the monitoring system"""
     logging.info(f"paper is Running: {self.running}")
-    # if not self.running:
-    #   return
+    if not self.running:
+      return
 
     is_valid, reason = self.evaluate_signal_for_entry(signal)
 
